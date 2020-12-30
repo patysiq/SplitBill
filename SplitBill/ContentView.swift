71f92fa2061cc2a237c4c2e3d999d4f1da563a40
Patricia <patysiq@yahoo.com.br>
2020-12-30 16:12:41 -0300

    Initial Commit

---
+++ SplitBill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplitBill
+//
+//  Created by PATRICIA S SIQUEIRA on 30/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by PATRICIA S SIQUEIRA on 30/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
